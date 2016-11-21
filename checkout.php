cebba62080fa9fd2888abd4e0c171000d27a1367
lhas <luizhrqas@gmail.com>
2016-11-21 05:49:55 -0200

    some update

--- checkout.php
+++ checkout.php
@@ -5,7 +5,12 @@ use PayPal\Api\ChargeModel;
 use PayPal\Api\Currency;
 use PayPal\Api\MerchantPreferences;
 use PayPal\Api\PaymentDefinition;
+use PayPal\Api\Patch;
+use PayPal\Api\PatchRequest;
 use PayPal\Api\Plan;
+use PayPal\Api\Agreement;
+use PayPal\Api\Payer;
+use PayPal\Common\PayPalModel;
 
 $apiContext = new \PayPal\Rest\ApiContext(
     new \PayPal\Auth\OAuthTokenCredential(
@@ -14,7 +19,7 @@ $apiContext = new \PayPal\Rest\ApiContext(
     )
 );
 
-function createPlan($name, $description, $frequency, $frequencyInterval, $cycles, $amountValue, $amountCurrency) {
+function createPlan($apiContext, $name, $description, $frequency, $frequencyInterval, $cycles, $amountValue, $amountCurrency) {
   $paymentDefinition = new PaymentDefinition();
 
   $paymentDefinition->setName('Regular Payments')
@@ -34,7 +39,7 @@ function createPlan($name, $description, $frequency, $frequencyInterval, $cycles
 
   $plan = setMerchantPreferences($plan, 'http://localhost/pagamentos-recorrentes-com-paypal');
 
-  return $plan;
+  return $plan->create($apiContext);
 }
 
 function setMerchantPreferences($plan, $baseUrl, $returnUrl = '/ExecuteAgreement.php?success=true', $cancelUrl = '/ExecuteAgreement.php?success=false') {
@@ -51,10 +56,53 @@ function setMerchantPreferences($plan, $baseUrl, $returnUrl = '/ExecuteAgreement
   return $plan;
 }
 
+function activatePlan($apiContext, $plan) {
+  $patch = new Patch();
+
+  $value = new PayPalModel('{
+     "state":"ACTIVE"
+   }');
+
+  $patch->setOp('replace')
+    ->setPath('/')
+    ->setValue($value);
+
+  $patchRequest = new PatchRequest();
+  $patchRequest->addPatch($patch);
+
+  $plan->update($patchRequest, $apiContext);
+
+  return Plan::get($plan->getId(), $apiContext);
+}
+
+function createAgreement($apiContext, $createdPlan) {
+  $agreement = new Agreement();
+
+  $agreement->setName('Base Agreement')
+    ->setDescription('Basic Agreement')
+    ->setStartDate('2019-06-17T9:45:04Z');
+
+  $plan = new Plan();
+  $plan->setId($createdPlan->getId());
+  $agreement->setPlan($plan);
+
+  $payer = new Payer();
+  $payer->setPaymentMethod('paypal');
+  $agreement->setPayer($payer);
+
+  return $agreement->create($apiContext);
+}
 
 // Criamos um plano chamado "User Premium".
 // Este plano será cobrado a cada 1 ($frequencyInterval) mês ($frequency) durante 12 meses ($cycles)
 // com um preço de R$ 50 ($amountCurrency e $amountValue).
-$userPremiumPlan = createPlan('User Premium Plan', 'A premium plan for our users on 0e1dev.com', 'Month', '1', '12', 50, 'BRL');
+$userPremiumPlan = createPlan($apiContext, 'User Premium Plan', 'A premium plan for our users on 0e1dev.com', 'Month', '1', '12', 50, 'BRL');
 
-var_dump($userPremiumPlan);
+// Agora vamos ativar o plano
+$userPremiumPlan = activatePlan($apiContext, $userPremiumPlan);
+
+$userPremiumAgreement = createAgreement($apiContext, $userPremiumPlan);
+
+redirectUser($userPremiumAgreement->getApprovalLink());
+
+var_dump($userPremiumAgreement);
